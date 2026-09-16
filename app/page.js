33bb49b43308a60273a22bead0e595e6f4106ba3
Sean McCulloch <99207694+SeanSpon@users.noreply.github.com>
2026-09-16 09:56:40 -0400

    Make homepage bare-bones and add profile photo

--- app/page.js
+++ app/page.js
@@ -1,20 +1,15 @@
-import Link from 'next/link';
 import Nav from './components/Nav';
 
 export default function Home() {
   return (
     <main>
       <Nav />
-      <section className="homePage pageWrap">
-        <div className="homeIntro">
+      <section className="homeBare pageWrap">
+        <img className="homePhoto" src="/sean-profile.jpg" alt="Sean McCulloch" />
+        <div>
           <p className="eyebrow">UNIVERSITY OF CINCINNATI · UNIVERSITY HONORS PROGRAM</p>
           <h1>Sean McCulloch</h1>
-          <p className="homeLead">Information Technology student studying Software Application Development.</p>
-          <p className="homeBlurb">This portfolio is a place for me to keep track of my experiences, interests, and growth throughout college.</p>
-          <div className="homeButtons">
-            <Link href="/about" className="primaryLink">About Me</Link>
-            <Link href="/year-in-review" className="secondaryLink">Year in Review</Link>
-          </div>
+          <p className="homeLine">Information Technology · Software Application Development</p>
         </div>
       </section>
     </main>
